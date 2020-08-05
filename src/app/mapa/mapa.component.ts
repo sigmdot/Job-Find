1007694d031fc27538a3be8d2628048c81db57fe
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-08-05 17:05:06 -0400

    "creado" mapa component

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Mapboxgl from 'mapbox-gl'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.sass']
 })
 export class MapaComponent implements OnInit {
-
+  Map: Mapboxgl.Map;
   constructor() { }
 
   ngOnInit(): void {
+    Mapboxgl.accessToken = environment.mapboxKey;
+    this.Map = new Mapboxgl.Map({
+      container: 'mapa-trabajo',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-77.04, 38.907],
+      zoom: 11.15
+    });
   }
 
 }
