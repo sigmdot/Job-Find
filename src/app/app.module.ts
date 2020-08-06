4b2a11f3808d8425342961fc151e5de017b39c7a
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-08-06 13:35:28 -0400

    creado perfil de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MapaComponent } from './mapa/mapa.component';
 import { ListasOfertasLaboralesComponent } from './listas-ofertas-laborales/listas-ofertas-laborales.component';
 import { ItemListaComponentComponent } from './item-lista-component/item-lista-component.component';
 import { UltimosTrabajosComponent } from './views/ultimos-trabajos/ultimos-trabajos.component';
+import { UserProfileComponent } from './views/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { UltimosTrabajosComponent } from './views/ultimos-trabajos/ultimos-traba
     MapaComponent,
     ListasOfertasLaboralesComponent,
     ItemListaComponentComponent,
-    UltimosTrabajosComponent
+    UltimosTrabajosComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
