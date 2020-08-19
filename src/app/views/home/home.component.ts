5a286b2ca72ed5e6681060e60e23fec863e24640
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-08-18 22:50:59 -0400

    Ubicación establecidad

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor() {
+    console.log('lol');
+    this.getUserPosition();
+    console.log(this.geoPosition);
+  }
+  geoPosition;
+  ngOnInit(): void {
+    console.log('soy inicio',this.geoPosition)
+  }
+  async getUserPosition(){
+    if (window.navigator.geolocation) {
+      this.geoPosition= await window.navigator.geolocation
+      .getCurrentPosition(position => this.geoPosition = position);
+     }
+     else{
+       alert('No es posible obtener tú ubicación, debido a que tu navegador no es compatible');
+     }
+  }
 }
