ef5626e116d93c77a365b2bd7b97fa0a6d653bed
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-09-02 19:11:42 -0400

    fix registro

--- src/app/formulario-datos-personales/formulario-datos-personales.component.ts
+++ src/app/formulario-datos-personales/formulario-datos-personales.component.ts
@@ -7,11 +7,49 @@ import {FormGroup,  FormControl, ReactiveFormsModule, FormsModule} from '@angula
   styleUrls: ['./formulario-datos-personales.component.scss']
 })
 export class FormularioDatosPersonalesComponent implements OnInit {
+  opcionesRegion =[
+    {
+      value: 1,
+      name: 'I - Tarapacá'
+    },
+    {
+      value: 2,
+      name: 'II - Antofagasta'
+    },
+    {
+      value: 3,
+      name: 'III - Atacama'
+    },
+    {
+      value: 4,
+      name: 'IV - Coquimbo'
+    },
+    {
+      value: 5,
+      name: 'V - Valparaíso'
+    },
+    {
+      value: 6,
+      name: "VI - Libertador Bernardo O'Higgins"
+    },
+    {
+      value: 7,
+      name: 'VII - Maule'
+    },
+    {
+      value: 8,
+      name: 'VIII - Concepción'
+    },
+    {
+      value: 9,
+      name: 'IX - Araucanía'
+    }
+
+  ]
   formDatosPersonales = new FormGroup({
     nombres: new FormControl(''),
     apellidos: new FormControl(''),
     region: new FormControl(''),
-    comuna: new FormControl(''),
     telefono: new FormControl(''),
     sobreMi: new FormControl('')
   });
