00bb2dbba3d677db14c4c71390ebf57c408ee940
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-09-03 01:56:50 -0400

    update Formulario subir foto

--- src/app/formulario-subir-foto/formulario-subir-foto.component.ts
+++ src/app/formulario-subir-foto/formulario-subir-foto.component.ts
@@ -6,9 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-subir-foto.component.scss']
 })
 export class FormularioSubirFotoComponent implements OnInit {
-
+  url = null;
   constructor() { }
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
 
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
+  }
   ngOnInit(): void {
   }
 
